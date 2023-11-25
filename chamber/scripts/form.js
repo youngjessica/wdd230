397a792dfc2a727040055da6083dc8a52fd361c1
youngjess <jessbyoung2012@gmail.com>
2023-11-24 23:03:06 -0500

    week 8 join

---
+++ chamber/scripts/form.js
@@ -0,0 +1,20 @@
+const pw1 = document.querySelector("#password");
+const pw2 = document.querySelector("#password2");
+const message = document.querySelector("#formmessage");
+
+kp2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+	if (pw1.value !== pw2.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.visibility = "show";
+		pw2.style.backgroundColor = "#fff0f3";
+		pw2.value = "";
+		pw2.focus();
+	} else {
+		message.style.display = "none";
+		pw2.style.backgroundColor = "#fff";
+		pw2.style.color = "#000";
+	}
+}
